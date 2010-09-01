60736c43b4fbba94d02c9f17ddbc7695bdfc5d78
alessandro.annini <alessandro.annini@924fe110-15ef-6dff-4a8d-bd5c9acccd05>
2010-09-01 08:09:40 +0000

    First update - source code only

---
+++ RandomFileLauncher/RFL.Logic.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RandomFileLauncher
+{
+    public partial class FrmMain
+    {
+        private void OsVersion()
+        {
+            if (_osInfo.Version.Major == 5)
+            {
+                btnBrowseFolder.BackColor = Color.Silver;
+                btnLaunch.BackColor = Color.Silver;
+                btnSearch.BackColor = Color.Silver;
+            }
+        }
+
+        private void BrowseFolder()
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _path = folderBrowserDialog1.SelectedPath;
+                tboxPath1.Text = _path;
+                tboxPath1.Refresh();
+                Search();
+            }
+        }
+
+        private void Search()
+        {
+            _files.Clear();
+            Waiting(true);
+
+            FileTypes();
+            if (_searchPattern.Count == 0)
+                _searchPattern.Add("*.*");
+
+            TextBox tmpTxBox = new TextBox();
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    tmpTxBox = (TextBox) control;
+
+                    if (!string.IsNullOrEmpty(tmpTxBox.Text))
+                    {
+                        _path = tmpTxBox.Text;
+                    }
+                }
+                else continue;
+
+                try
+                {
+                    List<string> tmpFiles = GetFiles(_path, _searchPattern, SearchOption.AllDirectories);
+                    _files.AddRange(tmpFiles);
+                }
+                catch (IOException ex)
+                {
+                    if (ex.Message != null) Debug.WriteLine(ex.Message);
+                }
+                
+            }
+            if (string.IsNullOrEmpty(_path))
+            {
+                MessageBox.Show("Choose a path", "", MessageBoxButtons.OKCancel,
+                                MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+            }
+
+            lblResult.Text = string.Format("{0} files found", _files.Count());
+            btnLaunch.Focus();
+            Waiting(false);
+        }
+
+        /// <summary>
+        /// Get all the files in a directory that match
+        /// the specified search pattern
+        /// </summary>
+        /// <param name="path">The directory to seach</param>
+        /// <param name="listOfSearchPatterns">The list of search patterns</param>
+        /// <param name="searchOption">Type of search</param>>
+        /// <returns>The list of files that match the specified pattern</returns>
+        private List<string> GetFiles(string path, IEnumerable<string> listOfSearchPatterns, SearchOption searchOption)
+        {
+            if (listOfSearchPatterns == null) throw new ArgumentNullException("listOfSearchPatterns");
+            _matchingFiles = new List<string>();
+
+            try
+            {
+                foreach (string pattern in listOfSearchPatterns)
+                {
+                    //add the the files that match our pattern to our list
+                    _matchingFiles.AddRange(Directory.GetFiles(path, pattern, searchOption));
+
+                }
+            }
+            catch (IOException ex)
+            {
+                if (ex.Message != null) Debug.WriteLine(ex.Message);
+            }
+
+            return _matchingFiles;
+        }
+
+        private void Launch()
+        {
+            if (_files.Count != 0)
+            {
+                int number = rnd.Next(0, _files.Count);
+                if (_alreadyOpened.IndexOf(number) >= 0)
+                    Launch();
+                _choosenRnd = number;
+                Process.Start(_files[number]);
+                _alreadyOpened.Add(number);
+            }
+        }
+
+        private void Delete()
+        {
+            if (_choosenRnd != -1)
+            {
+                File.Delete(_files[_choosenRnd]);
+                _choosenRnd = -1;
+
+            }
+        }
+
+        private void Back()
+        {
+            if (_choosenRnd != -1)
+            {
+                Process.Start(_files[_choosenRnd]);
+                _choosenRnd = -1;
+            }
+        }
+
+        private void Waiting(bool isWaiting)
+        {
+            if (isWaiting)
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                btnSearch.Text = "Searching...";
+                foreach (Control control in Controls)
+                {
+                    if (control is TextBox || control is Button || control is CheckBox)
+                        control.Enabled = false;
+                }
+                foreach (Control control in pnlControls.Controls)
+                {
+                    if (control is TextBox || control is Button || control is CheckBox)
+                        control.Enabled = false;
+                }
+            }
+            else
+            {
+                Cursor.Current = Cursors.Default;
+                btnSearch.Text = "Search";
+                foreach (Control control in Controls)
+                {
+                    if (control is TextBox || control is Button || control is CheckBox)
+                        control.Enabled = true;
+                }
+                foreach (Control control in pnlControls.Controls)
+                {
+                    if (control is TextBox || control is Button || control is CheckBox)
+                        control.Enabled = true;
+                }
+            }
+        }
+
+        private void AddControls()
+        {
+            if (_currentNewControl > MaNewControl) return;
+
+            SuspendLayout();
+            AdaptFormSize();
+            NewFormPosition();
+            ResumeLayout();
+
+            #region NEW DYNAMIC PATH TEXTBOX
+            _dynamicTextbox[_currentNewControl] = new TextBox
+                                                      {
+                                                          Name = "dynamictextbox" + _currentNewControl,
+                                                          Size = new Size(234, 20)
+                                                      };
+            int prevTboxY = tboxPath1.Location.Y;
+            if (_currentNewControl > 0)
+            {
+                prevTboxY = _dynamicTextbox[_currentNewControl - 1].Location.Y;
+            }
+            _dynamicTextbox[_currentNewControl].Location = new Point(12, prevTboxY + 26);
+            Controls.Add(_dynamicTextbox[_currentNewControl]); 
+            #endregion
+
+            #region NEW DYNAMIC PATH BUTTON
+            _dynamicButton[_currentNewControl] = new Button
+                                                     {
+                                                         Name = "dynamictextbox" + _currentNewControl,
+                                                         Tag = _currentNewControl,
+                                                         Size = new Size(28, 20)
+                                                     };
+            int prevBtnY = tboxPath1.Location.Y;
+            if (_currentNewControl > 0)
+            {
+                prevBtnY = _dynamicButton[_currentNewControl - 1].Location.Y;
+            }
+            _dynamicButton[_currentNewControl].Location = new Point(252, prevBtnY + 26);
+            _dynamicButton[_currentNewControl].Text = "...";
+            _dynamicButton[_currentNewControl].BackColor = Color.White;
+            _dynamicButton[_currentNewControl].UseVisualStyleBackColor = true;
+            _dynamicButton[_currentNewControl].Click += DynBtnBrowseFolderClick;
+            Controls.Add(_dynamicButton[_currentNewControl]); 
+            #endregion
+
+            _currentNewControl += 1;
+        }
+
+        private void AdaptFormSize()
+        {
+            int formHeight = Size.Height;
+            Size = new Size(298, formHeight + 26);
+        }
+
+        private void NewFormPosition()
+        {
+            int loc = Location.Y;
+            Location = new Point(Location.X, loc - 26);
+        }
+
+        private void DynBtnBrowseFolderClick(object sender, EventArgs e)
+        {
+            Button tmpBtn = new Button();
+            if (sender is Button)
+                tmpBtn = (Button)sender;
+
+            DynBrowseFolder(tmpBtn);
+        }
+
+        private void DynBrowseFolder(Button tmpBtn)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _path = folderBrowserDialog1.SelectedPath;
+                if (_path != null) _dynamicTextbox[Convert.ToInt32(tmpBtn.Tag)].Text = _path;
+                _dynamicTextbox[Convert.ToInt32(tmpBtn.Tag)].Refresh();
+                Search();
+            }
+        }
+    }
+}
---
+++ RandomFileLauncher/RFL.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RandomFileLauncher
+{
+    public partial class FrmMain : Form
+    {
+        #region DECLARATIONS
+        readonly OperatingSystem _osInfo = Environment.OSVersion;
+        private string _path = string.Empty;
+        List<string> _searchPattern = new List<string>();
+        List<string> _files = new List<string>();
+        private List<string> _matchingFiles;
+        Random rnd = new Random();
+        int _choosenRnd = -1;
+        List<int> _alreadyOpened = new List<int>();
+
+        private readonly Button[] _dynamicButton = new Button[3];
+        private readonly TextBox[] _dynamicTextbox = new TextBox[3];
+        private const int MaNewControl = 2;
+        private int _currentNewControl;
+        #endregion
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            lblVersion.Text = string.Format("V. {0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            PositionReporterEdge();
+            OsVersion();
+        }
+
+        #region OPTIONS
+        private void FileTypes()
+        {
+            _searchPattern.Clear();
+
+            if (chbAudio.Checked)
+            {
+                _searchPattern.Add("*.mp3");
+                _searchPattern.Add("*.wav");
+                _searchPattern.Add("*.flac");
+                _searchPattern.Add("*.m4a");
+                _searchPattern.Add("*.wma");
+            }
+            if (chbVideo.Checked)
+            {
+                _searchPattern.Add("*.mpeg");
+                _searchPattern.Add("*.mpg");
+                _searchPattern.Add("*.avi");
+                _searchPattern.Add("*.wmv");
+                _searchPattern.Add("*.mp4");
+                _searchPattern.Add("*.mov");
+                _searchPattern.Add("*.mkv");
+                _searchPattern.Add("*.rm");
+                _searchPattern.Add("*.ram");
+                _searchPattern.Add("*.flv");
+            }
+            if (chbPicture.Checked)
+            {
+                _searchPattern.Add("*.jpeg");
+                _searchPattern.Add("*.jpg");
+                _searchPattern.Add("*.gif");
+                _searchPattern.Add("*.png");
+            }
+            if (chbText.Checked)
+            {
+                _searchPattern.Add("*.txt");
+                _searchPattern.Add("*.pdf");
+                _searchPattern.Add("*.odt");
+                _searchPattern.Add("*.doc");
+                _searchPattern.Add("*.docx");
+            }
+        }
+
+        private void ChbAlwaysOnTopCheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = chbAlwaysOnTop.Checked;
+        }
+
+        private void ChbTransparentCheckedChanged(object sender, EventArgs e)
+        {
+            Opacity = chbTransparent.Checked ? 0.4 : 1;
+        }
+        #endregion
+
+        #region BUTTONS
+        private void BtnBrowseFolderClick(object sender, EventArgs e)
+        {
+            BrowseFolder();
+        }
+
+        private void BtnLaunchClick(object sender, EventArgs e)
+        {
+            if (_files.Count != 0)
+                Launch();
+            else
+                MessageBox.Show("No files to open.", "No files");
+        }
+
+        private void BtnSearchClick(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tboxPath1.Text))
+                Search();
+            else
+                MessageBox.Show("Please, choose a path.", "Path missing");
+        }
+
+        private void BtnLaunchKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (_files.Count != 0)
+                Launch();
+            else
+                MessageBox.Show("No files to open.", "No files");
+        }
+
+        private void FrmMain_HelpButtonClicked(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show("I'm working on it!", "Nothing yet...");
+        }
+
+        private void BtnDeleteClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Delete();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please, close the file before.", "File in use");
+            }
+        }
+
+        private void BtnAddTextboxClick(object sender, EventArgs e)
+        {
+            AddControls();
+        }
+        #endregion
+
+        private void PositionReporterEdge()
+        {
+            int screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            int screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+
+            int newHeight = screenHeight - ClientSize.Height - 125;
+            int newWidth = screenWidth - ClientSize.Width - 20;
+
+            Location = new Point(newWidth, newHeight);
+        }
+    }
+    //NOTE http://www.keithrull.com/2008/02/02/HowToSearchADirectoryForFilesUsingMultipleSearchPatterns.aspx
+}
